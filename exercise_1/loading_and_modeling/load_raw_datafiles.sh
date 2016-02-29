f2a89a6a39c92ae59f7402a7e06bc4c988d10e3b
bethieppart <bethieppart@gmail.com>
2016-02-28 19:42:21 -0800

    Create load_raw_datafiles.sh

---
+++ exercise_1/loading_and_modeling/load_raw_datafiles.sh
@@ -0,0 +1,5 @@
+cd /data
+rm load_data_lake.sh
+wget https://raw.githubusercontent.com/bethieppart/W205/master/exercise_1/loading_and_modeling/load_data_lake.sh
+chmod +x /data/load_data_lake.sh
+/data/load_data_lake.sh
