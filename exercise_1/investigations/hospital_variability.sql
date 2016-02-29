9b65a9e712af4281b68de363eda4216a579ca2c7
bethieppart <bethieppart@gmail.com>
2016-02-28 20:26:35 -0800

    Update hospital_variability.sql

--- exercise_1/investigations/hospital_variability.sql
+++ exercise_1/investigations/hospital_variability.sql
@@ -2,7 +2,7 @@ SELECT m.measure_name,
 VARIANCE(s.score) as variance 
 FROM combined_trans s
 JOIN measures_trans m
-ON m.measure_id = s.measure_id
+ON m.measures_id = s.measures_id
 GROUP BY m.measure_name 
 ORDER BY variance DESC 
 LIMIT 10; 
