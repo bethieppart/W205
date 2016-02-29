542e63ad58cf447b6b7adebfe7acd02580736e43
bethieppart <bethieppart@gmail.com>
2016-02-28 20:37:16 -0800

    Update all_transforms.sh

--- exercise_1/transforming/all_transforms.sh
+++ exercise_1/transforming/all_transforms.sh
@@ -3,3 +3,4 @@ hive -f /data/W205/exercise_1/transforming/measures_trans.sql
 hive -f /data/W205/exercise_1/transforming/surveys_trans.sql
 hive -f /data/W205/exercise_1/transforming/effective_trans.sql
 hive -f /data/W205/exercise_1/transforming/readmissions_trans.sql
+hive -f /data/W205/exercise_1/transforming/combined_trans.sql
