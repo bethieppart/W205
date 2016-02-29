d2060b1ef2fd011a306af27e4e296c95541be059
bethieppart <bethieppart@gmail.com>
2016-02-28 19:31:06 -0800

    Update all_transforms.sh

--- exercise_1/transforming/all_transforms.sh
+++ exercise_1/transforming/all_transforms.sh
@@ -1,5 +1,5 @@
-hive -f ./hospitals_trans.sql
-hive -f ./measures_trans.sql
-hive -f ./surveys_trans.sql
-hive -f ./effective_trans.sql
-hive -f ./readmissions_trans.sql
+hive -f /data/W205/exercise_1/transforming/hospitals_trans.sql
+hive -f /data/W205/exercise_1/transforming/measures_trans.sql
+hive -f /data/W205/exercise_1/transforming/surveys_trans.sql
+hive -f /data/W205/exercise_1/transforming/effective_trans.sql
+hive -f /data/W205/exercise_1/transforming/readmissions_trans.sql
