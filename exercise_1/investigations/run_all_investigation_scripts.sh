508326f878dfaf0135aa0ae9a27c743dd81aab0e
bethieppart <bethieppart@gmail.com>
2016-02-29 00:18:26 -0800

    Update run_all_investigation_scripts.sh

--- exercise_1/investigations/run_all_investigation_scripts.sh
+++ exercise_1/investigations/run_all_investigation_scripts.sh
@@ -1,4 +1,4 @@
-hive -f /data/W205/exercise_1/investigations/best_hospitals.sql
-hive -f /data/W205/exercise_1/investigations/best_states.sql
-hive -f /data/W205/exercise_1/investigations/hospital_variability.sql
-hive -f /data/W205/exercise_1/investigations/hospitals_and_patients.sql
+hive -f /data/W205/exercise_1/investigations/best_hospitals.sql > best_hospitals_output
+hive -f /data/W205/exercise_1/investigations/best_states.sql > best_states_output
+hive -f /data/W205/exercise_1/investigations/hospital_variability.sql > hospital_variability_output
+hive -f /data/W205/exercise_1/investigations/hospitals_and_patients.sql > hospitals_and_patients_output
