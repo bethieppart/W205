f895134f9bcc1efbbf8c2e19f1e105116579fde4
bethieppart <bethieppart@gmail.com>
2016-02-28 23:31:39 -0800

    Create run_all_investigation_scripts.sh

---
+++ exercise_1/investigations/run_all_investigation_scripts.sh
@@ -0,0 +1,4 @@
+hive -f /data/W205/exercise_1/investigations/best_hospitals.sql
+hive -f /data/W205/exercise_1/investigations/best_states.sql
+hive -f /data/W205/exercise_1/investigations/hospital_variability.sql
+hive -f /data/W205/exercise_1/investigations/hospitals_and_patients.sql
